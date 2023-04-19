11172d03060ae989c97ca2e041ad2f28939fa50a
Noor Chasib <noorchasib@gmail.com>
2023-04-18 22:21:07 -0700

    Update backend index.js to include middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
-const bodyParser = require("body-parser");
 
-const landingRouter = require("./src/routes/landing");
+// Import Routes
+import landingRoutes from "./src/routes/landing/landing.js";
 
-app.use(bodyParser.json());
+// Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:8000",
+  }),
+);
+app.use(cookieParser());
 
-app.use("/", landingRouter);
+// Routes
+app.use("/", landingRoutes);
 
+// Start Server
 app.listen(3000, () => console.log("Server started on port 3000"));
