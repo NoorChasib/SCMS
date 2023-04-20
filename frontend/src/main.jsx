7a2480a4ff8d6f5d6305d8ce6dc7ae6eb01d8365
Noor Chasib <noorchasib@gmail.com>
2023-04-19 20:29:18 -0700

    create authContext for cookies and accessTokens storing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { MantineProvider, createEmotionCache } from "@mantine/core";
+import { AuthContextProvider } from "./context/authContext";
+
+const appendCache = createEmotionCache({ key: "mantine", prepend: false });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <MantineProvider
+      emotionCache={appendCache}
+      theme={{ fontFamily: "Source Sans Pro, sans-serif" }}
+    >
+      <App />
+    </MantineProvider>
+  </AuthContextProvider>,
 );
