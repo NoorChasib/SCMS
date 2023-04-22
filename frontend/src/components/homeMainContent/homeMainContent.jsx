318eb256991868940fd20da0cbea297c9b4700f0
Noor Chasib <noorchasib@gmail.com>
2023-04-22 16:29:47 -0700

    update homeMainContent to dynamically add cameras and link to their respective pages

--- frontend/src/components/homeMainContent/homeMainContent.jsx
+++ frontend/src/components/homeMainContent/homeMainContent.jsx
@@ -1,18 +1,27 @@
 import { Grid, Skeleton, Container } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/authContext";
+import { useContext } from "react";
 
 const HomeMainContent = () => {
+  const { userData } = useContext(AuthContext);
+  const { cameras } = userData || {};
+
   return (
     <Container size="2xl" py="md">
       <Grid grow gutter="xl" gutterMd={50}>
-        <Grid.Col md={10} lg={10}>
-          <Skeleton height={450} radius="lg" />
-        </Grid.Col>
-        <Grid.Col md={6} lg={6}>
-          <Skeleton height={450} radius="lg" />
-        </Grid.Col>
-        <Grid.Col md={6} lg={6}>
-          <Skeleton height={450} radius="lg" />
-        </Grid.Col>
+        {cameras &&
+          cameras.map((camera) => (
+            <Grid.Col key={camera.id} lg={5}>
+              <Link to={`/camera/${camera.id}`}>
+                <Skeleton
+                  className="hover:scale-20 relative cursor-pointer hover:transform hover:shadow-xl"
+                  height={450}
+                  radius="lg"
+                />
+              </Link>
+            </Grid.Col>
+          ))}
       </Grid>
     </Container>
   );
