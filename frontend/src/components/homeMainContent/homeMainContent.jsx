7ee85c141cb2ec90f9d91cf4fc50f009c52f803d
Noor Chasib <noorchasib@gmail.com>
2023-04-22 01:38:43 -0700

    create skeleton grid layout for camera and home main content components

--- frontend/src/components/homeMainContent/homeMainContent.jsx
+++ frontend/src/components/homeMainContent/homeMainContent.jsx
@@ -1,7 +1,21 @@
-import { Text } from "@mantine/core";
+import { Grid, Skeleton, Container } from "@mantine/core";
 
 const HomeMainContent = () => {
-  return <Text>Home</Text>;
+  return (
+    <Container size="2xl" py="md">
+      <Grid grow gutter="xl" gutterMd={50}>
+        <Grid.Col md={10} lg={10}>
+          <Skeleton height={450} radius="lg" />
+        </Grid.Col>
+        <Grid.Col md={6} lg={6}>
+          <Skeleton height={450} radius="lg" />
+        </Grid.Col>
+        <Grid.Col md={6} lg={6}>
+          <Skeleton height={450} radius="lg" />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
 };
 
 export default HomeMainContent;
