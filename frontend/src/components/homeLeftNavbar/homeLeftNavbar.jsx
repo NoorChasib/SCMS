ae97e0f826454cfd791d51db12998ece904910ab
Noor Chasib <noorchasib@gmail.com>
2023-04-21 22:03:29 -0700

    near complete homeLeftNavbar look

--- frontend/src/components/homeLeftNavbar/homeLeftNavbar.jsx
+++ frontend/src/components/homeLeftNavbar/homeLeftNavbar.jsx
@@ -1,14 +1,71 @@
-import { Navbar, Text } from "@mantine/core";
+import { Navbar, Divider, Button } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { IconVideo, IconCamera } from "@tabler/icons-react";
 
-const HomeLeftNavbar = ({ opened }) => {
+const HomeLeftNavbar = ({ opened, setOpened }) => {
   return (
-    <Navbar
-      p="md"
-      hiddenBreakpoint="sm"
-      hidden={!opened}
-      width={{ sm: 200, lg: 300 }}
-    >
-      <Text>Application navbar</Text>
+    <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 220, lg: 300 }}>
+      <div className="h-full bg-blue-300 p-4 text-white">
+        <Navbar.Section>
+          <Link to="/">
+            <Button
+              leftIcon={<IconCamera />}
+              radius="lg"
+              size="md"
+              fullWidth
+              uppercase
+              variant="subtle"
+              color="dark"
+              onClick={() => setOpened((o) => !o)}
+            >
+              All Cameras
+            </Button>
+          </Link>
+        </Navbar.Section>
+
+        <Divider my="sm" />
+        <Navbar.Section my="xl">
+          <Button
+            leftIcon={<IconVideo />}
+            radius="lg"
+            size="md"
+            fullWidth
+            variant="subtle"
+            color="dark"
+            onClick={() => setOpened((o) => !o)}
+          >
+            Camera 1
+          </Button>
+        </Navbar.Section>
+
+        <Navbar.Section my="xl">
+          <Button
+            leftIcon={<IconVideo />}
+            radius="lg"
+            size="md"
+            fullWidth
+            variant="subtle"
+            color="dark"
+            onClick={() => setOpened((o) => !o)}
+          >
+            Camera 2
+          </Button>
+        </Navbar.Section>
+
+        <Navbar.Section my="xl">
+          <Button
+            leftIcon={<IconVideo />}
+            radius="lg"
+            size="md"
+            fullWidth
+            variant="subtle"
+            color="dark"
+            onClick={() => setOpened((o) => !o)}
+          >
+            Camera 3
+          </Button>
+        </Navbar.Section>
+      </div>
     </Navbar>
   );
 };
