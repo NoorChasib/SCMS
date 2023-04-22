bc54962563278118dbca6311efa60257381ca519
Noor Chasib <noorchasib@gmail.com>
2023-04-22 00:47:16 -0700

    remove Logout icon from Header

--- frontend/src/components/topHeader/topHeader.jsx
+++ frontend/src/components/topHeader/topHeader.jsx
@@ -1,11 +1,11 @@
 import { Header, Text, MediaQuery, Burger, Group } from "@mantine/core";
 import { Link } from "react-router-dom";
-import { IconFingerprint, IconLogout } from "@tabler/icons-react";
+import { IconFingerprint } from "@tabler/icons-react";
 
 const TopHeader = ({ opened, setOpened }) => {
   return (
     <Header height={{ base: 60 }} withBorder={false} fz="lg">
-      <div className="flex h-full items-center bg-blue-700 text-white shadow-sm sm:justify-start sm:pl-4">
+      <div className="flex h-full items-center bg-blue-700 text-white shadow-lg sm:justify-start sm:pl-4">
         <MediaQuery largerThan="sm" styles={{ display: "none" }}>
           <Burger
             opened={opened}
@@ -27,13 +27,7 @@ const TopHeader = ({ opened, setOpened }) => {
           </Group>
         </Link>
 
-        <Text className="flex-1 text-center font-bold">Overview</Text>
-
-        <Link to="/logout">
-          <div className="pr-4 sm:pl-14">
-            <IconLogout size={30} />
-          </div>
-        </Link>
+        <Text className="flex-1 text-center font-bold sm:mr-24">Overview</Text>
       </div>
     </Header>
   );
