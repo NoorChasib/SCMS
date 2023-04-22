2dadd15bb0eaf3e564c93d48edaeff3cfacbee9c
Noor Chasib <noorchasib@gmail.com>
2023-04-21 18:13:02 -0700

    synchronize media query breakpoints for mobile responsiveness

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,19 @@ const appendCache = createEmotionCache({ key: "mantine", prepend: false });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
     <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily: "Inter, sans-serif",
+        breakpoints: {
+          xs: "30em",
+          sm: "48em",
+          md: "64em",
+          lg: "74em",
+          xl: "90em",
+        },
+      }}
       emotionCache={appendCache}
-      theme={{ fontFamily: "Inter, sans-serif" }}
     >
       <App />
     </MantineProvider>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xl: { max: "90em" },
+        lg: { max: "74em" },
+        md: { max: "64em" },
+        sm: { max: "48em" },
+        xs: { max: "30em" },
+      },
+    },
   },
   plugins: [],
 };
