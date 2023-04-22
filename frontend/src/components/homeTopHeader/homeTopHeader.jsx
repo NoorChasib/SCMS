5f3219eb12be1a42dbd01bfd7e842e6636ef0d31
Noor Chasib <noorchasib@gmail.com>
2023-04-21 22:03:44 -0700

    complete homeLeftNavbar look

--- frontend/src/components/homeTopHeader/homeTopHeader.jsx
+++ frontend/src/components/homeTopHeader/homeTopHeader.jsx
@@ -1,33 +1,33 @@
-import {
-  Header,
-  Text,
-  MediaQuery,
-  Burger,
-  Group,
-  Avatar,
-  Title,
-} from "@mantine/core";
-import logo from "/logo.svg";
+import { Header, Text, MediaQuery, Burger, Group, Avatar } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { IconFingerprint } from "@tabler/icons-react";
 
 const HomeTopHeader = ({ opened, setOpened }) => {
   return (
-    <Header height={{ base: 60 }}>
-      <div className="flex h-full items-center justify-center bg-gray-200 sm:justify-start sm:pl-4">
+    <Header height={{ base: 60 }} withBorder={false}>
+      <div className="flex h-full items-center bg-blue-700 text-white shadow-sm sm:justify-start sm:pl-4">
         <MediaQuery largerThan="sm" styles={{ display: "none" }}>
           <Burger
             opened={opened}
             onClick={() => setOpened((o) => !o)}
-            size="sm"
+            color="white"
+            size="md"
             mr="xl"
           />
         </MediaQuery>
 
-        <Group align="center">
-          <Avatar src={logo} alt="no image here" />
-          <Text>SCMS</Text>
-        </Group>
+        <Link to="/">
+          <Group className="pl-4 sm:p-0">
+            <div className="flex items-center justify-center gap-2">
+              <IconFingerprint size={30} />
+              <Text fz="xl" className="font-bold">SCMS</Text>
+            </div>
+          </Group>
+        </Link>
 
-        <Text>Overview</Text>
+        <Text fz="xl" className="flex-1 text-center font-bold">
+          Overview
+        </Text>
       </div>
     </Header>
   );
