196966e465cb4e80592f170bb49df82a31aed8b0
Noor Chasib <noorchasib@gmail.com>
2023-04-21 19:09:48 -0700

    create overview home page

---
+++ frontend/src/pages/HomePage/Home.jsx
@@ -0,0 +1,21 @@
+import { AppShell } from "@mantine/core";
+import HomeLeftNavbar from "../../components/homeLeftNavbar/homeLeftNavbar";
+import HomeTopHeader from "../../components/homeTopHeader/homeTopHeader";
+import HomeMainContent from "../../components/homeMainContent/homeMainContent";
+import { useState } from "react";
+
+const Home = () => {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <AppShell
+      navbarOffsetBreakpoint="sm"
+      navbar={<HomeLeftNavbar opened={opened} />}
+      header={<HomeTopHeader opened={opened} setOpened={setOpened} />}
+    >
+      <HomeMainContent />
+    </AppShell>
+  );
+};
+
+export default Home;
