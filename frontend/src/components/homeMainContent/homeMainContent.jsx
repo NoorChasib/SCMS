5b3ff3f3767fbe3552eea757bf8d2087f3b48f2d
Noor Chasib <noorchasib@gmail.com>
2023-04-22 21:50:46 -0700

    add dark mode and light mode hovers

--- frontend/src/components/homeMainContent/homeMainContent.jsx
+++ frontend/src/components/homeMainContent/homeMainContent.jsx
@@ -1,11 +1,13 @@
 import { Grid, Skeleton, Container } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext";
+import ThemeContext from "../../contexts/themeContext";
 import { useContext } from "react";
 
 const HomeMainContent = () => {
   const { userData } = useContext(AuthContext);
   const { cameras } = userData || {};
+  const { darkMode } = useContext(ThemeContext);
 
   return (
     <Container size="2xl" py="md">
@@ -15,7 +17,9 @@ const HomeMainContent = () => {
             <Grid.Col key={camera.id} lg={5}>
               <Link to={`/camera/${camera.id}`}>
                 <Skeleton
-                  className="hover:scale-20 relative cursor-pointer hover:transform hover:shadow-xl"
+                  className={`hover:scale-25 relative cursor-pointer hover:transform hover:shadow-lg ${
+                    darkMode ? "hover:shadow-gray-500" : "hover:shadow-gray-300"
+                  }`}
                   height={450}
                   radius="lg"
                 />
