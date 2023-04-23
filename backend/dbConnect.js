ad8fbc06aa91db0ec84fcead71f7f68e84186da5
Noor Chasib <noorchasib@gmail.com>
2023-04-23 15:51:52 -0700

    stop node from converting date automatically

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  dateStrings: true,
 });
 
 // Define JWT configuration options using environment variables
