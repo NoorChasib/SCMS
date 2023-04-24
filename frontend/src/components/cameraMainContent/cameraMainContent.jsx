6780423bb4809352a68097711d0cd5caf2e76287
Noor Chasib <noorchasib@gmail.com>
2023-04-23 17:07:22 -0700

    update cameraMainContent to show specific camera video feed

--- frontend/src/components/cameraMainContent/cameraMainContent.jsx
+++ frontend/src/components/cameraMainContent/cameraMainContent.jsx
@@ -1,11 +1,31 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
 import { Grid, Skeleton, Container } from "@mantine/core";
+import { DataContext } from "../../contexts/dataContext";
+import VideoPlayer from "../liveVideo/liveVideo";
 
 const CameraMainContent = () => {
+  const { cameras, cameraInfo } = useContext(DataContext);
+  const { id } = useParams(); // Get the camera id from the URL
+
+  // Find the corresponding camera and cameraInfo
+  const camera = cameras && cameras.find((cam) => cam.id === parseInt(id));
+  const currentCameraInfo = cameraInfo.find(
+    (info) => info.camera_id === parseInt(id),
+  );
+  const videoUrl = currentCameraInfo ? currentCameraInfo.url : "";
+
   return (
     <Container size="2xl" py="md">
       <Grid grow gutter="xl" gutterMd={50}>
         <Grid.Col md={10} lg={10}>
-          <Skeleton height={450} radius="lg" />
+          {camera && (
+            <VideoPlayer
+              key={videoUrl}
+              src={videoUrl}
+              type="application/x-mpegURL"
+            />
+          )}
         </Grid.Col>
         <Grid.Col md={6} lg={6}>
           <Skeleton height={450} radius="lg" />
